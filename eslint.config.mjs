4b091877e177fd42f9ef7d9fddaf367fe2ccadab
AndalYLP <andal089.az@gmail.com>
2025-02-08 09:56:10 -0500

    deleted old eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -33,7 +33,18 @@ export default [
                     ignoreVoid: true,
                 },
             ],
-            "@typescript-eslint/no-unused-vars": "warn",
+            "@typescript-eslint/no-unused-vars": [
+                "warn",
+                {
+                    "args": "all",
+                    "argsIgnorePattern": "^_",
+                    "caughtErrors": "all",
+                    "caughtErrorsIgnorePattern": "^_",
+                    "destructuredArrayIgnorePattern": "^_",
+                    "varsIgnorePattern": "^_",
+                    "ignoreRestSiblings": true
+                }
+            ],
             "@typescript-eslint/explicit-function-return-type": "off",
             "@typescript-eslint/interface-name-prefix": "off",
             "@typescript-eslint/no-empty-function": "off",
