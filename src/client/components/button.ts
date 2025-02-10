1883123778e489ea209bde95b9ec8fe38aa31ba2
AndalYLP <andal089.az@gmail.com>
2025-02-10 10:46:54 -0500

    added dialog after button clicked

--- src/client/components/button.ts
+++ src/client/components/button.ts
@@ -2,7 +2,10 @@ import { BaseComponent, Component } from "@flamework/components";
 import { OnStart } from "@flamework/core";
 import { Logger } from "@rbxts/log";
 import Signal from "@rbxts/signal";
+import { DialogController } from "client/controllers/dialog";
 import { ShakeController } from "client/controllers/shake";
+import { events } from "client/network";
+import { ForceStop } from "client/ui/components/dialog";
 
 interface ButtonModel extends Model {
 	Button: MeshPart;
@@ -26,6 +29,7 @@ export class ButtonComponent
 	constructor(
 		private readonly logger: Logger,
 		private readonly shakeController: ShakeController,
+		private readonly dialogController: DialogController,
 	) {
 		super();
 		logger.Info("loaded");
@@ -33,9 +37,45 @@ export class ButtonComponent
 		this.clicker = this.instance.HitBox.ClickDetector;
 	}
 
+	private dialog(): void {
+		ForceStop.Fire(1);
+
+		this.dialogController.sendDialogWait(2, "...", 0.3, true);
+
+		task.wait(5);
+
+		this.dialogController.sendDialogWait(
+			2,
+			'Ok, you can go back to the normal "world".',
+			0.02,
+			true,
+		);
+
+		task.wait(3);
+
+		this.dialogController.sendDialogWait(2, "Good luck, you will need it.", 0.02);
+
+		task.wait(3);
+
+		this.dialogController.sendDialogWait(
+			3,
+			"Didn't tell you, but if you really want to escape",
+			0.02,
+			true,
+		);
+
+		task.wait(2);
+
+		this.dialogController.sendDialogWait(3, "You will need to find the real portal.", 0.02);
+	}
+
 	private onClick(): void {
+		this.dialog();
+
 		this.shakeController.shakeCamera(5, 0.1, 6);
 		blink.Fire();
+
+		events.game.ready();
 	}
 
 	public onStart(): void {
