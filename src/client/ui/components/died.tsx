65c19cf409e897cc67f051904fb3f932db349da4
AndalYLP <andal089.az@gmail.com>
2025-02-10 14:55:31 -0500

    improved died component

--- src/client/ui/components/died.tsx
+++ src/client/ui/components/died.tsx
@@ -1,4 +1,4 @@
-import { useEventListener } from "@rbxts/pretty-react-hooks";
+import { useEventListener, useMotion } from "@rbxts/pretty-react-hooks";
 import React, { useState } from "@rbxts/react";
 import { LocalPlayer } from "client/constants/player";
 
@@ -7,27 +7,33 @@ if (!LocalPlayer.Character) LocalPlayer.CharacterAdded.Wait();
 const humanoid = LocalPlayer.Character?.FindFirstChildOfClass("Humanoid")!;
 
 const diedMessages: Array<string> = [
-	"You choosed this.",
-	"My world looks great now right?",
-	"I didn't want to do this.",
+	"You chose this.",
+	"My world looks great now, doesn't it?",
+	"I never wanted to do this...",
+	"This is how it ends.",
+	"Don't blame me for your choices.",
 ];
 
 export function Died(): React.ReactNode {
 	const [visible, setVisible] = useState(false);
 	const [message, setMessage] = useState("");
+	const [transparency, motion] = useMotion(1);
 
 	useEventListener(humanoid.Died, () => {
 		setMessage(diedMessages[math.random(0, diedMessages.size() - 1)]);
 		setVisible(true);
+		motion.spring(0);
 	});
 
 	return (
 		<textlabel
+			Transparency={transparency}
 			Visible={visible}
 			AnchorPoint={new Vector2(0.5, 0.5)}
 			Position={UDim2.fromScale(0.5, 0.5)}
 			Size={UDim2.fromScale(1, 1)}
 			Text={message}
+			TextColor3={new Color3(1, 1, 1)}
 			TextScaled={true}
 			BackgroundColor3={new Color3()}
 		></textlabel>
