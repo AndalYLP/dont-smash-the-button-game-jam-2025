1259df20a7d444ad9a5c1064cf779dbc49038aef
AndalYLP <andal089.az@gmail.com>
2025-02-09 20:03:04 -0500

    added rock component, class and service to create rocks

---
+++ src/server/services/game/rock/class.ts
@@ -0,0 +1,57 @@
+import { Players, Workspace } from "@rbxts/services";
+import { CharacterRig } from "utils/player";
+
+export class Rock {
+	private readonly totalSpawns!: number;
+	private character!: CharacterRig;
+	private throttle = false;
+
+	constructor(
+		private readonly player: Player,
+		private readonly instance: BasePart,
+		private readonly target?: CFrame,
+	) {
+		this.totalSpawns = Workspace.RockSpawns.GetChildren().size();
+		this.character = player.Character as CharacterRig;
+
+		instance.Touched.Connect(hit => {
+			if (this.throttle) return;
+			if (!Players.GetPlayerFromCharacter(hit.Parent)) return;
+			this.character.Humanoid.TakeDamage(15);
+
+			this.throttle = true;
+		});
+	}
+
+	private getSpawnPoint(): CFrame {
+		const index = tostring(math.random(1, this.totalSpawns)) as "1";
+
+		return Workspace.RockSpawns[index].CFrame;
+	}
+
+	private moveToSpawn(): void {
+		const spawn = this.getSpawnPoint();
+
+		this.instance.CFrame = spawn;
+	}
+
+	public launch(): void {
+		this.moveToSpawn();
+
+		task.wait();
+
+		this.instance.Parent = Workspace;
+
+		const target =
+			this.target ?? this.character.HumanoidRootPart.CFrame.mul(new CFrame(30, -30, 0));
+
+		this.instance.CFrame = CFrame.lookAt(this.instance.Position, target.Position);
+
+		for (let i = 0; i <= 400; i++) {
+			this.instance.Position = this.instance.CFrame.Lerp(target, i / 400).Position;
+			task.wait(0.015);
+		}
+
+		this.instance.Destroy();
+	}
+}
